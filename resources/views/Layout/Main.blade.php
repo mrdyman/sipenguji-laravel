077bda549753cf06878504bdd30bacb162fc475f
mrdyman <andimardimansaputra@gmail.com>
2021-06-07 06:55:44 +0800

    tambah menu pada sidebar(on progress)

--- resources/views/Layout/Main.blade.php
+++ resources/views/Layout/Main.blade.php
@@ -80,8 +80,8 @@
                     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                         <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-                        <li class="nav-item menu-open">
-                            <a href="#" class="nav-link active">
+                        <li class="nav-item menu-openz">
+                            <a href="#" class="nav-link">
                                 <i class="nav-icon fas fa-tachometer-alt"></i>
                                 <p>
                                     Dashboard
@@ -90,9 +90,25 @@
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="./index3.html" class="nav-link active">
+                                    <a href="{{ url('') }}" class="nav-link active">
                                         <i class="far fa-circle nav-icon"></i>
-                                        <p>Dashboard</p>
+                                        <p>Home</p>
+                                    </a>
+                                </li>
+                            </ul>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="{{ url('/polyline')}}" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Polyline</p>
+                                    </a>
+                                </li>
+                            </ul>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="{{ url('/floydwarshall')}}" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Floyd-Warshall</p>
                                     </a>
                                 </li>
                             </ul>
